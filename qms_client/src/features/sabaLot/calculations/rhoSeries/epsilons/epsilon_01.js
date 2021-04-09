748754ca606dfa295916183dd403f5c65eafa7f7
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-04-09 21:47:21 +0800

    So I will handle the brakes very soon. But those epsilons are indeed very very useful as well.

--- qms_client/src/features/sabaLot/calculations/rhoSeries/epsilons/epsilon_01.js
+++ qms_client/src/features/sabaLot/calculations/rhoSeries/epsilons/epsilon_01.js
@@ -26,11 +26,52 @@ export const eps0102 = rhocxt => {
 
   const metesco = !R.isNil(wodltqc);
 
+  const esfnct =
+    '卦若無財及財落空其中之一也會導致一切營謀不堪許';
+
+  const escont = '卦若無財及落空,一切營謀不堪許';
+
+  const esbkn = '闡奧歌章';
+  const eschn = '求財買賣章';
+  const escnid = 'eps-0102';
+  const esfnpt = '卦若無財';
+
   return {
     metesco,
-    escnid: 'eps-0102',
-    escont: '卦若無財及落空',
-    esfnct: '卦若無財及（財）落空',
-    esfnpt: '卦若無財'
+    escnid,
+    esfnpt,
+    escont,
+    esfnct,
+    esbkn,
+    eschn
+  }
+}
+
+export const eps0103 = rhocxt => {
+
+  const wodltqc = R.find(
+    R.equals(rpcmi['dlt-qc']),
+    rhhfs);
+
+  const metesco = !R.isNil(wodltqc);
+
+  const esfnct =
+    '卦若無財及財落空其中之一也會導致一切營謀不堪許';
+
+  const escont = '卦若無財及落空,一切營謀不堪許';
+
+  const esbkn = '闡奧歌章';
+  const eschn = '求財買賣章';
+  const escnid = 'eps-0103';
+  const esfnpt = '卦若財落空';
+
+  return {
+    metesco,
+    escnid,
+    esfnpt,
+    escont,
+    esfnct,
+    esbkn,
+    eschn
   }
 }
