e98808283f165da2a8861e9e79260daece1bf7f0
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-04-10 22:50:19 +0800

    Now move to rho 6.

--- qms_client/src/App.js
+++ qms_client/src/App.js
@@ -1,12 +1,12 @@
 import JSONTree from 'react-json-tree';
 import * as R from 'ramda';
-import { rhocs_3 } from
-'./features/sabaLot/calculations/rhoSeries/rhos/rho_4';
+import { rhocs_4 } from
+'./features/sabaLot/calculations/rhoSeries/rhos/rho_5';
 
 function showTree(){
   return (
     <div>
-      <JSONTree data={rhocs_3}/>
+      <JSONTree data={rhocs_4}/>
     </div>
   )
 }
--- qms_client/src/features/sabaLot/calculations/rhoSeries/rhos/rho_5.js
+++ qms_client/src/features/sabaLot/calculations/rhoSeries/rhos/rho_5.js
@@ -1,13 +1,83 @@
 import * as R from 'ramda';
 import * as RA from 'ramda-adjunct';
 import {
-  rhocs_4
-} from './rho_4';
-import {
   getElre,
   idxOfTrunk,
   itemOfTrunk
 } from '../../twigs/twig_1';
+import {
+  nattos
+} from './rho_1';
+import {
+  getRcxtvByLhn
+} from './rho_2';
+import {
+  rhocs_2
+} from './rho_3';
+import {
+  rhocs_3
+} from './rho_4';
+
+
+const getRhocs_4 = () => {
+
+  const mapFn = rhocxt_3 => {
+
+    const rhocxt_2 = R.find(
+      R.propEq('lhname', rhocxt_3.lhname),
+      rhocs_2);
+
+    const natto = R.find(
+      R.propEq('gshori', rhocxt_3.rhshn),
+      nattos);
+
+    if(R.isNil(natto)){
+      throw new Error(
+        'Natto should not be nil.');
+    }
+
+    const rhshel = natto.gshele;
+
+    if(R.isNil(rhshel)){
+      throw new Error(
+        'RHSHEL should not be nil.');
+    }
+
+    return {
+      ...rhocxt_2,
+      ...rhocxt_3,
+      rhshel
+    }
+  }
+
+  try {
+
+    const rhocs_4a = R.map(mapFn, rhocs_3);
+
+    const rhocs_4b = R.sortBy(
+      R.prop('rglhi'), rhocs_4a);
+
+    return rhocs_4b;
+  }
+  catch(err){
+    console.error(err);
+    throw new Error('Cannot get RHOCS_4.');
+  }
+}
+
+export const rhocs_4 = getRhocs_4();
+
+export const getRcxt4ByLhn = lhname => {
+  try {
+    return getRcxtvByLhn(
+      lhname, rhocs_4, 4);
+  }
+  catch(err){
+    console.error(err);
+    throw new Error(
+      'Cannot get RHOCXT_4 by long hook name.')
+  }
+}
 
 // Rho focus raw map
 const rfram = {
