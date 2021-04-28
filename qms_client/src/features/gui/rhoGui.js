e4b83879d7b135fb9aad30828e78910c5d439694
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-04-28 20:50:00 +0800

    Now proceed to better 2FA.

--- qms_client/src/features/gui/rhoGui.js
+++ qms_client/src/features/gui/rhoGui.js
@@ -1,8 +1,8 @@
 import JSONTree from 'react-json-tree';
 import * as R from 'ramda';
-import { rhocs_4 } from
-  '../calculations/rhoSeries/rhos/rho_08';
+import { rhocs_2 } from
+  '../calculations/rhoSeries/rhos/rho_03';
 
 export default function RhoGui() {
-  return <JSONTree data={rhocs_4}/>
+  return <JSONTree data={rhocs_2}/>
 }
