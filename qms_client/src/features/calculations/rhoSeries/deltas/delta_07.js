74ffeb6f2b00949765db97ff502e597ac8e07a83
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-05-02 23:11:11 +0800

    So will move to at least array style or even sentence style if necessary for deltas.

--- qms_client/src/features/calculations/rhoSeries/deltas/delta_07.js
+++ qms_client/src/features/calculations/rhoSeries/deltas/delta_07.js
@@ -1,4 +1,5 @@
 import * as R from 'ramda';
+import * as RA from 'ramda-adjunct';
 import {
   utFilterByPropEq
 } from '../../utils/util_05';
@@ -6,7 +7,7 @@ import {
   isValidDfccn
 } from '../rhos/rho_08';
 
-// Get cross by focus
+// Get cross list by focus
 export const getClbfc = (dfccn, slcrl) => {
 
   E.cknwa(slcrl, 'slcrl');
@@ -40,6 +41,96 @@ export const getClbpl = (dplcn, slcrl) => {
 }
 
 export const getRjkcr = slcrl => {
+
+  try{
+    return utFindByPropTrue(slcrl, 'isRjk');
+  }
+  catch(err){
+    console.error(err);
+    throw new Error('Cannot get [rjkcr].');
+  }
+}
+
+export const getRqncr = slcrl => {
+
+  try{
+    return utFindByPropTrue(slcrl, 'isRqn');
+  }
+  catch(err){
+    console.error(err);
+    throw new Error('Cannot get [rqncr].');
+  }
+}
+
+export const getRkgcr = slcrl => {
+
+  try{
+    return utFindByPropTrue(slcrl, 'isRkg');
+  }
+  catch(err){
+    console.error(err);
+    throw new Error('Cannot get [rkgcr].');
+  }
+}
+
+// [wbdfc] is wheat bowl delta focus chinese
+// [chdfc] is cheese delta focus chinese
+// [clbpm] is cross list by focus movement
+export const getClbfm =
+  (wbdfc, chdfc, slcrl) => {
+
+  const mapFn = lhcdwi => {
+
+    // Iso downward index cross list
+    const idicl = utFilterByPropEq(
+      'lhcdwi', lhcdwi, slcrl);
+
+    if(idicl.length == 1){
+      return null;
+    }
+    else if(crosses.length == 2){
+
+      // Wheat bowl iso downward index cross
+      const wbics = utFindByPropTrue(
+        'isWbocr', idicl);
+
+      const chics = utFindByPropFalse(
+        'isWbocr', idicl);
+
+      E.cknwo(wbics, 'wbics');
+      E.cknwo(chics, 'chics');
+
+      // Is wheat bowl match the requied focus
+      const isWbmfc = wbics.dfccn == wbdfc;
+      const isChmfc = chics.dfccn == chdfc;
+
+      // Is matching focus movement
+      const isMfcm = isWbmfc && isChmfc;
+      if(isMfcm){
+        return { wbics, chics };
+      }
+      else {
+        return null;
+      }
+    }
+    else {
+      throw new Error(
+        'It must be at least one cross'
+        + 'matched for various [lhcdwi].');
+    }
+  }
+
+  try{
+    const clbfm = R.compose(
+      RA.compact,
+      R.map(mapFn))
+    (R.range(0, 6));
+  }
+  catch(err){
+    console.error(err);
+    throw new Error(
+      'Cannot get [clbfm].');
+  }
 
 
 }
