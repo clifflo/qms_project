489f59913a7b6a129e20fc204634c4e363172b4f
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-05-06 22:29:05 +0800

    Perhaps I have gone to the wrong direction. Let's change by using array conditions.

--- qms_client/src/features/calculations/rhoSeries/deltas/delta_11.js
+++ qms_client/src/features/calculations/rhoSeries/deltas/delta_11.js
@@ -7,10 +7,44 @@ import {
 } from './delta_09';
 
 // [cbfwf] is cross by focus with rho face card
-export const getCbfwr = (dfcal, rfacd, slcrl) => {
+export const getCbfwr =
+  (dfcal, rfacd, slcrl) => {
 
   const findFn = slcros => {
 
-    const dfccn = getD
+    const dfccn = getDfcba(dfcal);
+
+    const matchFocus = slcros.dfccn == dfccn;
+
+    const matchJack = rfacd == 'Jack'
+      && slcros.isRjk;
+
+    const matchKing = rfacd == 'King'
+      && slcros.isRkg;
+
+    const matchQueen = rfacd == 'Queen'
+      && slcros.isRqn;
+
+    const matchFace = R.anyPass([
+      matchJack,
+      matchKing,
+      matchQueen
+    ]);
+
+    return matchFace && matchFocus;
+  }
+
+  try{
+    const result = R.find(findFn, slcrl);
+    return result;
+  }
+  catch(err){
+    console.error(err);
+    throw new Error('Cannot get [cbfwr].');
   }
 }
+
+export const getCbfrp =
+  (dfcal, rfacd, ) => {
+
+  }
