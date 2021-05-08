8964b55fbb07e9f43c6c391e1c950e2ba33b5e5a
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-05-08 17:59:23 +0800

    So rho files are refactored quite nicely.

--- qms_client/src/features/calculations/rhoSeries/rhoFiles/rhoFile_03.js
+++ qms_client/src/features/calculations/rhoSeries/rhoFiles/rhoFile_03.js
@@ -7,7 +7,7 @@ import {
   shortHookContextSet
 } from './rhoFile_01';
 
-const longHookParagraph = R.join(',', [
+export const longHookParagraph = R.join(',', [
   '乾為天,天風姤,天山遯,天地否,風地觀,山地剝,火地晉,地風升',
   '火天大有,坎為水,水澤節,水雷屯,水火既濟,澤火革,雷火豐',
   '地火明夷,地水師,艮為山,山火賁,山天大畜,山澤損,火澤睽',
@@ -19,7 +19,8 @@ const longHookParagraph = R.join(',', [
   '澤水困,澤地萃,澤山咸,水山蹇,地山謙,雷山小過,雷澤歸妹'
 ]);
 
-const getShortHookContextByGenericShortHookAlternative =
+export const
+  getShortHookContextByGenericShortHookAlternative =
   genericShortHookAlternative => {
 
   checkNilWithString(
@@ -34,88 +35,3 @@ const getShortHookContextByGenericShortHookAlternative =
 
   return shortHookContext;
 }
-
-const getRhoContextSet_1 = () => {
-
-  const rhoContextMapFunction_1 =
-    longHookSentence => {
-
-    let externalShortHookAlternative;
-    let internalShortHookAlternative;
-    let longHookName;
-
-    if(longHookSentence == ''){
-      throw new Error(
-        'Sentence should not be empty string.')
-    }
-
-    if(longHookSentence[1] == '為'){
-
-      externalShortHookAlternative =
-        longHookSentence[2];
-
-      internalShortHookAlternative =
-        longHookSentence[2];
-
-      longHookName = '純' + longHookSentence[0];
-    }
-    else {
-      externalShortHookAlternative =
-        longHookSentence[0];
-
-      internalShortHookAlternative =
-        longHookSentence[1];
-
-      longHookName = R.drop(2, longHookSentence);
-    }
-
-    E.checkNilWithString(
-      externalShortHookAlternative,
-      'External short hook alternate'
-    );
-
-    E.checkNilWithString(
-      internalShortHookAlternative,
-      'Internal short hook alternate'
-    )
-
-    const externalShortHookIndex =
-      getGenericShortHookIndexByAlternative(
-        externalShortHookAlternative);
-
-    const internalShortHookIndex =
-      getGenericShortHookIndexByAlternative(
-        internalShortHookAlternative);
-
-    const eshori =
-      gshoro[externalShortHookIndex];
-
-    const ishori =
-      gshoro[internalShortHookIndex];
-
-    const lhidx =
-      (externalShortHookIndex * 8) +
-      internalShortHookIndex;
-
-    return {
-      externalShortHookAlternative,
-      internalShortHookAlternative,
-      eshori,
-      ishori,
-      longHookName,
-      externalShortHookIndex,
-      internalShortHookIndex,
-      lhidx
-    }
-  }
-
-  return R.compose(
-    R.map(rhoContextMapFunction_1),
-    R.split(',')
-  )
-  (longHookParagraph);
-
-}
-
-export const rhoContextSet_1 =
-  getRhoContextSet_1();
---
+++ qms_client/src/features/calculations/rhoSeries/rhoFiles/rhoFile_04.js
@@ -0,0 +1,120 @@
+import * as R from 'ramda';
+import * as RA from 'ramda-adjunct';
+import {
+  longHookParagraph,
+  getShortHookContextByGenericShortHookAlternative
+} from './rhoFile_04';
+
+const rhoContextMapFunction_1 =
+  longHookSentence => {
+
+  let externalShortHookAlternative;
+  let internalShortHookAlternative;
+  let longHookName;
+
+  if(longHookSentence == ''){
+    throw new Error(
+      'Sentence should not be empty string.')
+  }
+
+  if(longHookSentence[1] == '為'){
+
+    externalShortHookAlternative =
+      longHookSentence[2];
+
+    internalShortHookAlternative =
+      longHookSentence[2];
+
+    longHookName = '純' + longHookSentence[0];
+  }
+  else {
+
+    externalShortHookAlternative =
+      longHookSentence[0];
+
+    internalShortHookAlternative =
+      longHookSentence[1];
+
+    longHookName = R.drop(2, longHookSentence);
+  }
+
+  E.checkNilWithString(
+    externalShortHookAlternative,
+    'External short hook alternate'
+  );
+
+  E.checkNilWithString(
+    internalShortHookAlternative,
+    'Internal short hook alternate'
+  )
+
+  const externalShortHookContext =
+    getShortHookContextByGenericShortHookAlternative(
+      externalShortHookAlternative);
+
+  const internalShortHookContext =
+    getShortHookContextByGenericShortHookAlternative(
+      internalShortHookAlternative);
+
+  const externalShortHookIndex =
+    externalShortHookContext
+    .genericShortHookIndex;
+
+  const internalShortHookIndex =
+    internalShortHookContext
+    .genericShortHookIndex;
+
+  const externalShortHookOriginal =
+    externalShortHookContext
+    .genericShortHookOriginal;
+
+  const internalShortHookOriginal =
+    intertalShortHookContext
+    .generticShortHookOriginal;
+
+  const externalShortHookEnglish =
+    externalShortHookContext
+    .genericShortHookEnglish;
+
+  const internalShortHookEnglish =
+    internalShortHookContext
+    .genericShortHookEnglish;
+
+  const longHookIndex =
+    (externalShortHookContext
+      .genericShortHookIndex * 8) +
+    internalShortHookContext
+      .genericShortHookIndex;
+
+  return {
+    longHookName,
+    longHookIndex,
+    externalShortHookOriginal,
+    externalShortHookAlternative,
+    externalShortHookEnglish,
+    internalShortHookOriginal,
+    internalShortHookAlternative,
+    internalShortHookEnglish
+  }
+}
+
+const buildRhoContextSet_1 = () => {
+
+  try{
+    return R.compose(
+      R.map(rhoContextMapFunction_1),
+      R.split(',')
+    )
+    (longHookParagraph);
+  }
+  catch(errorMessage){
+    console.error(errorMessage);
+    throw new Error(
+      'Cannot build Rho Context Set 1.')
+  }
+
+
+}
+
+export const rhoContextSet_1 =
+  getRhoContextSet_1();
