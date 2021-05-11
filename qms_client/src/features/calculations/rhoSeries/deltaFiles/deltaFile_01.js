dfb6cd4dd771fc3bd20d179a3c514b909dd1a220
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-05-11 12:03:26 +0800

    More on twigs and then to deltas.

---
+++ qms_client/src/features/calculations/rhoSeries/deltaFiles/deltaFile_01.js
@@ -0,0 +1,11 @@
+import * as R from 'ramda';
+import * as RA from 'ramda-adjunct';
+
+export const deltaFocusContextSet = [
+  {
+    _type: 'Delta focus context',
+    deltaFocusChineseName: '丙官鬼',
+    deltaFocusEnglishName: 'Delta ghost',
+    deltaFocusElementalRelation: 'Hack'
+  }
+]
---
+++ qms_client/src/features/calculations/twigFiles/twigFile_04.js
@@ -0,0 +1,36 @@
+import * as R from 'ramda';
+import * as RA from 'ramda-adjunct';
+
+export const twigElementalRelationContext = [
+  {
+    twigElementalRelationChinese: '比和',
+    twigElementalRelationEnglish:
+      'Twig elemental relation draws with',
+    twigElementalRelationIndex: 0
+  },
+  {
+    twigElementalRelationChinese: '生他',
+    twigElementalRelationEnglish:
+      'Twig elemental relation producing',
+    twigElementalRelationIndex: 1
+  },
+  {
+    twigElementalRelationChinese: '剋他',
+    twigElementalRelationEnglish:
+      'Twig elemental relation hacking towards',
+    twigElementalRelationIndex: 2
+
+  },
+  {
+    twigElementalRelationChinese: '被他剋',
+    twigElementalRelationEnglish:
+      'Twig elemental relation hacked by',
+    twigElementalRelationIndex: 3
+  },
+  {
+    twigElementalRelationChinese: '被他生',
+    twigElementalRelationEnglish:
+      'Twig elemental relation produced by',
+    twigElementalRelationIndex: 4
+  }
+]
