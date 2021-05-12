71c05fc1d37aebaf823289fa50131e5dbaae256f
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-05-12 15:23:52 +0800

    More on delta paladins.

---
+++ qms_client/src/features/calculations/rhoSeries/deltaFiles/deltaFile_06.js
@@ -0,0 +1,41 @@
+import * as R from 'ramda';
+import * as RA from 'ramda-adjunct';
+
+export const deltaPaladinContextSet = [
+  {
+    _type: 'Delta paladin context',
+    deltaPaladinChinese: '丙玄武',
+    deltaPaladinEnglish: 'Delta paladin turtle.',
+    deltaPaladinIndex: 0
+  },
+  {
+    _type: 'Delta paladin context',
+    deltaPaladinChinese: '丙白虎',
+    deltaPaladinEnglish: 'Delta paladin tiger.',
+    deltaPaladinIndex: 1
+  },
+  {
+    _type: 'Delta paladin context',
+    deltaPaladinChinese: '丙螣蛇',
+    deltaPaladinEnglish: 'Delta paladin cobra.',
+    deltaPaladinIndex: 2
+  },
+  {
+    _type: 'Delta paladin context',
+    deltaPaladinChinese: '丙勾陳',
+    deltaPaladinEnglish: 'Delta paladin unicorn.',
+    deltaPaladinIndex: 3
+  },
+  {
+    _type: 'Delta paladin context',
+    deltaPaladinChinese: '丙朱雀',
+    deltaPaladinEnglish: 'Delta paladin phoenix.',
+    deltaPaladinIndex: 4
+  },
+  {
+    _type: 'Delta paladin context',
+    deltaPaladinChinese: '丙青龍',
+    deltaPaladinEnglish: 'Delta paladin dragon.',
+    deltaPaladinIndex : 5
+  }
+]
