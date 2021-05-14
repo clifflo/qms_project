4588d1d9342726e989f29b4e34d115f89307900f
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-05-14 17:44:31 +0800

    So move on chosen of day then.

--- qms_client/src/features/calculations/twigFiles/twigFile_08.js
+++ qms_client/src/features/calculations/twigFiles/twigFile_08.js
@@ -3,63 +3,104 @@ import * as RA from 'ramda-adjunct';
 
 export const chosenContextSet = [
   {
+    _type: 'Chosen context',
     chosenChineseName: '長生',
     chosenEnglishName: 'Chosen life',
     chosenIndex: 0
   },
   {
+    _type: 'Chosen context',
     chosenChineseName: '沐浴',
     chosenEnglishName: 'Chosen bath',
     chosenIndex: 1
   },
   {
+    _type: 'Chosen context',
     chosenChineseName: '冠帶',
     chosenEnglishName: 'Chosen belt',
     chosenIndex: 2
   },
   {
+    _type: 'Chosen context',
     chosenChineseName: '臨官',
-    chosenEnglishName: 'Chosen boss',
+    chosenEnglishName: 'Chosen officer',
     chosenIndex: 3
   },
   {
+    _type: 'Chosen context',
     chosenChineseName: '帝旺',
     chosenEnglishName: 'Chosen emperor',
     chosenIndex: 4
   },
   {
+    _type: 'Chosen context',
     chosenChineseName: '衰',
     chosenEnglishName: 'Chosen tired',
     chosenIndex: 5
   },
   {
+    _type: 'Chosen context',
     chosenChineseName: '病',
     chosenEnglishName: 'Chosen sick',
     chosenIndex: 6
   },
   {
+    _type: 'Chosen context',
     chosenChineseName: '長死',
     chosenEnglishName: 'Chosen death',
     chosenIndex: 7
   },
   {
+    _type: 'Chosen context',
     chosenChineseName: '墓',
     chosenEnglishName: 'Chosen grave',
     chosenIndex: 8
   },
   {
+    _type: 'Chosen context',
     chosenChineseName: '絕',
     chosenEnglishName: 'Chosen vanished',
     chosenIndex: 9
   },
   {
+    _type: 'Chosen context',
     chosenChineseName: '胎',
     chosenEnglishName: 'Chosen fetal',
     chosenIndex: 10
   },
   {
+    _type: 'Chosen context',
     chosenChineseName: '養',
     chosenEnglishName: 'Chosen birth',
     chosenIndex: 11
   }
+];
+
+export const
+  chosenForBranchContextSet_1 = [
+  {
+    chosenBranchElementalEnglish:
+      'Twig elemental metal',
+    chosenForBranchStartPosition: 7
+  },
+  {
+    chosenBranchElementalEnglish:
+      'Twig elemental wood',
+    chosenForBranchStartPosition: 1
+  },
+  {
+    chosenBranchElementalEnglish:
+      'Twig elemental water',
+    chosenForBranchStartPosition: 4
+  },
+  {
+    chosenBranchElementalEnglish:
+      'Twig elemental fire',
+    chosenForBranchStartPosition: 10
+  },
+  {
+    chosenBranchElementalEnglish:
+      'Twig elemental earth',
+    chosenForBranchStartPosition: 10
+  },
 ];
--- qms_client/src/features/calculations/twigFiles/twigFile_09.js
+++ qms_client/src/features/calculations/twigFiles/twigFile_09.js
@@ -1,27 +1,4 @@
-const chosenForBranchMovementContextList = [
-  {
-    chosenBranchElementalEnglish:
-      'Twig elemental metal',
-    chosenForBranchMovement: 7
-  },
-  {
-    chosenBranchElementalEnglish:
-      'Twig elemental wood',
-    chosenForBranchMovement: 1
-  },
-  {
-    chosenBranchElementalEnglish:
-      'Twig elemental water',
-    chosenForBranchMovement: 4
-  },
-  {
-    chosenBranchElementalEnglish:
-      'Twig elemental fire',
-    chosenForBranchMovement: 10
-  },
-  {
-    chosenBranchElementalEnglish:
-      'Twig elemental earth',
-    chosenForBranchMovement: 10
-  },
-];
+export const
+  mapFunctionOfChosenForBranchContextSet_2 =
+  chosenSourceTwigElemental,
+  
