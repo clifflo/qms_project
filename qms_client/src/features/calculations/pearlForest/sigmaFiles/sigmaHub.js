079938af57b8504c47854f829bfc1721ea2e158c
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-05-22 22:50:05 +0800

    So after think twice Mr. Lau final decision is to remove the sigma calculations for once and ever.

--- qms_client/src/features/calculations/pearlForest/sigmaFiles/sigmaHub.js
+++
@@ -1,6 +0,0 @@
-export * from './sigmaFile_01';
-export * from './sigmaFile_02';
-export * from './sigmaFile_03';
-export * from './sigmaFile_04';
-export * from './sigmaFile_05';
-export * from './sigmaFile_06';
