a8adaccaec6536be64104a88cb6564a4314368d6
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-05-22 13:02:16 +0800

    More on sigma. Also the peachtree client is quite useful.

--- qms_client/src/features/calculations/pearlForest/sigmaFiles/sigmaFile_02.js
+++ qms_client/src/features/calculations/pearlForest/sigmaFiles/sigmaFile_02.js
@@ -6,5 +6,27 @@ export const sigmaPlainMonthRegex =
 export const sigmaPlainBranchRegex =
   '[子丑寅卯辰巳午未申酉戌亥鼠牛虎兔龍蛇馬羊猴雞狗犬豬]';
 
-export consg sigmaPlainTrunkRegex =
+export const sigmaPlainTrunkRegex =
   '[甲乙丙丁戊己庚辛壬癸]';
+
+export const sigmaPlainSeasonRegex =
+  '[春夏秋冬]';
+
+export const getSigmaCombinedRegex =
+  (sigmaPlainRegex_1,
+  sigmaPlainRegex_2) => {
+
+  checkNilWithString(
+  	sigmaPlainRegex_1,
+  	'sigmaPlainRegex_1');
+
+  checkNilWithString(
+  	sigmaPlainRegex_2,
+  	'sigmaPlainRegex_2');
+
+  const sigmaCombinedRegex =
+    `(${sigmaPlainRegex_1}+)(?:.*?)`
+    + `(${sigmaPlainRegex_2}+)`;
+
+  return sigmaCombinedRegex;
+}
