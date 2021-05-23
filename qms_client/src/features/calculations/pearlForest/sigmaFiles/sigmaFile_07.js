94bfa8597e68f7345122affa31a3152721cf9502
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-05-23 21:32:32 +0800

    Let's finish the sigma.

---
+++ qms_client/src/features/calculations/pearlForest/sigmaFiles/sigmaFile_07.js
@@ -0,0 +1,137 @@
+export const sigmaBrake_31 = {
+  sigmaBrakeIndex: 31,
+  sigmaBrakeName: '巳喝散神',
+  sigmaBrakeDescription: [
+    '官事若發動，喝散不由人。'
+  ],
+  sigmaBrakeTopics: [
+    'Lawsuit'
+  ],
+  sigmaBrakeSource: 'Sigma season',
+  sigmaBrakeTarget: 'Sigma cross branch',
+  sigmaBrakeOriginalSentence: [
+    '春巳、夏居申，秋豬、冬到寅。'
+  ],
+  sigmaBrakeContext: {
+    '春': '巳',
+    '夏': '申',
+    '秋': '亥',
+    '冬': '寅'
+  }
+}
+
+export const sigmaBrake_32 = {
+  sigmaBrakeIndex: 32,
+  sigmaBrakeName: '巳日喝散',
+  sigmaBrakeDescription: [
+    '遇之婚姻忌，官司卻不成。'
+  ],
+  sigmaBrakeTopics: [
+    'Lawsuit',
+    'Marriage'
+  ],
+  sigmaBrakeSource: 'Sigma day trunk',
+  sigmaBrakeTarget: 'Sigma cross branch',
+  sigmaBrakeOriginalSentence: [
+    '甲己逢寅、乙庚申，丙辛、',
+    '戊癸向蛇尋，丁壬亥上。'
+  ],
+  sigmaBrakeContext: {
+    '甲': '寅',
+    '乙': '申',
+    '丙': '巳',
+    '丁': '亥',
+    '戊': '巳',
+    '己': '寅',
+    '庚': '申',
+    '辛': '巳',
+    '壬': '亥',
+    '癸': '巳'
+  }
+}
+
+export const sigmaBrake_33 = {
+  sigmaBrakeIndex: 33,
+  sigmaBrakeName: '巳開獄神',
+  sigmaBrakeDescription: [
+    '卦中若得此爻動，長久囚人放出來。'
+  ],
+  sigmaBrakeTopics: [
+    'Lawsuit'
+  ],
+  sigmaBrakeSource: 'Sigma season',
+  sigmaBrakeTarget: 'Sigma cross branch',
+  sigmaBrakeOriginalSentence: [
+    '獄神夏馬、春兔挨，秋雞、冬鼠獄門開。'
+  ],
+  sigmaBrakeContext: {
+    '春': '卯',
+    '夏': '午',
+    '秋': '酉',
+    '冬': '子'
+  }
+}
+
+export const sigmaBrake_34 = {
+  sigmaBrakeIndex: 34,
+  sigmaBrakeName: '巳活曜星',
+  sigmaBrakeDescription: [
+    '孕婦胎動則產疾病，遇此辰發動則痊。'
+  ],
+  sigmaBrakeTopics: [
+    'Illness'
+  ],
+  sigmaBrakeSource: 'Sigma month original',
+  sigmaBrakeTarget: 'Sigma cross branch',
+  sigmaBrakeOriginalSentence: [
+    '正卯、二辰、三月巳，四午、五未、',
+    '六居申，七酉、八戌、九亥上，十子、',
+    '十一丑、十二寅。'
+  ],
+  sigmaBrakeContext: {
+    '正': '卯',
+    '二': '辰',
+    '三': '巳',
+    '四': '午',
+    '五': '未',
+    '六': '申',
+    '七': '酉',
+    '八': '戌',
+    '九': '亥',
+    '十': '子',
+    '十一': '丑',
+    '十二': '寅'
+  }
+}
+
+export const sigmaBrake_35 = {
+  sigmaBrakeIndex: 35,
+  sigmaBrakeName: '巳活曜星',
+  sigmaBrakeDescription: [
+    '孕婦胎動則產疾病，遇此辰發動則痊。'
+  ],
+  sigmaBrakeTopics: [
+    'Illness'
+  ],
+  sigmaBrakeSource: 'Sigma month original',
+  sigmaBrakeTarget: 'Sigma cross branch',
+  sigmaBrakeOriginalSentence: [
+    '正卯、二辰、三月巳，四午、五未、',
+    '六居申，七酉、八戌、九亥上，十子、',
+    '十一丑、十二寅。'
+  ],
+  sigmaBrakeContext: {
+    '正': '卯',
+    '二': '辰',
+    '三': '巳',
+    '四': '午',
+    '五': '未',
+    '六': '申',
+    '七': '酉',
+    '八': '戌',
+    '九': '亥',
+    '十': '子',
+    '十一': '丑',
+    '十二': '寅'
+  }
+}
