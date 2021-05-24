22e09d0f179b5d86d29450694813b4c60c688c56
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-05-24 23:18:32 +0800

    Once added the year branch then next step is to differentiate with the target of cross trunk or cross branch. And then I will split that up to each branch and trunk and use this table to put into the dual cross set context to complete the project.

---
+++ qms_client/src/features/calculations/pearlForest/sigmaFiles/sigmaFile_10.js
@@ -0,0 +1,98 @@
+import * as R from 'ramda';
+import {
+  sigmaBrakeContextPartialSetOfDayBranch
+} from './sigmaMiniHub_01';
+import {
+  checkNilWithString,
+  checkNilWithUntypedObject,
+  checkNilWithArray,
+  throwFunctionalError
+} from './_utilityHubBySigma';
+import {
+  getTauMonthBranchChineseFromOriginal
+} from './_tauHubBySigma';
+import {
+  isValidTwigBranchChinese
+} from './_twigHubBySigma';
+
+export const handleSigmaBoltOfDayBranch =
+  (sigmaBrakeTargetOfDayBranch,
+  sigmaBoltOfDayBranch) => {
+
+  try{
+
+    const sigmaSourceNutOfDayBranch =
+      sigmaBoltOfDayBranch[0];
+
+    checkNilWithString(
+      sigmaSourceNutOfDayBranch,
+      'sigmaSourceNutOfDayBranch');
+
+    const validTwigBranchChinese =
+      isValidTwigBranchChinese(
+        sigmaSourceNutOfDayBranch);
+
+    if(!validTwigBranchChinese){
+      throw new Error(
+        'Sigma source nut of day branch is not valid.');
+    }
+
+    const sigmaTargetNutOfDayBranch =
+      sigmaBoltOfDayBranch[1];
+
+    checkNilWithString(
+      sigmaTargetNutOfDayBranch,
+      'sigmaTargetNutOfDayBranch');
+
+    const rawSigmaBoltContextOfDayBranch = {
+      _type: 'SigmaBoltContextOfDayBranch',
+      sigmaSourceNutOfDayBranch
+    };
+
+    if(sigmaBrakeTargetOfDayBranch
+      == 'sigma-brake-target-cross-branch'){
+      const finalSigmaBoltContextOfDayBranchForCrossBranch =
+      {
+        ...rawSigmaBoltContextOfDayBranch,
+        sigmaTargetCrossBranchNutForDayBranch:
+        sigmaTargetNutOfDayBranch
+      };
+
+      return finalSigmaBoltContextOfDayBranchForCrossBranch;
+    }
+    else if(sigmaBrakeTargetOfDayBranch
+      == 'sigma-brake-target-cross-trunk'){
+      const finalSigmaBoltContextOfDayBranchForCrossTrunk =
+      {
+        ...rawSigmaBoltContextOfDayBranch,
+        sigmaTargetCrossTrunkNutForDayBranch:
+        sigmaTargetNutOfDayBranch
+      }
+
+      return finalSigmaBoltContextOfDayBranchForCrossTrunk;
+    }
+    else if(sigmaBrakeTargetOfDayBranch
+      == 'sigma-brake-target-cross-twig-combo'){
+      const finalSigmaBoltContextOfDayBranchForCrossTwigCombo =
+      {
+        ...rawSigmaBoltContextOfDayBranch,
+        sigmaTargetCrossTwigComboNutForDayBranch:
+        sigmaTargetNutOfDayBranch
+      }
+
+      return finalSigmaBoltContextOfDayBranchForCrossTwigCombo;
+    }
+    else{
+      throw new Error(
+        'The sigma target should be just '
+        + 'cross trunk, cross branch or '
+        + 'cross twig combo.');
+    }
+  }
+  catch(errorMessage){
+    console.error(errorMessage);
+    throwFunctionalError(
+      'handleSigmaBoltOfDayBranch');
+  }
+
+}
