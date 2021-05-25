ec32adc89e3c381d5ab35fb6ce789215917e7361
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-05-25 16:17:07 +0800

    So only year branch left.

--- qms_client/src/features/gui/rhoGui.js
+++ qms_client/src/features/gui/rhoGui.js
@@ -1,10 +1,10 @@
 import JSONTree from 'react-json-tree';
 import * as R from 'ramda';
 import {
-  coatedCrunchSetOfDayTrunk
+  coatedCrunchSetOfMonth
 } from '../calculations/calculationsHub';
 
 export default function RhoGui() {
   return <JSONTree
-    data={coatedCrunchSetOfDayTrunk}/>
+    data={coatedCrunchSetOfMonth}/>
 }
