014b2773eecfd4dfb6aef15e0f355f8c315561f5
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-06-15 10:48:48 +0800

    So month and day should not be nut.

--- qms_client/src/features/calculations/pearlForest/epsilonFiles/epsilonFile_10.js
+++ qms_client/src/features/calculations/pearlForest/epsilonFiles/epsilonFile_10.js
@@ -1,5 +1,23 @@
 import {
   coatedCrunchSetOfMonth
 } from './_sigmaHubOfEpsilon';
+import {
+  findItemByStringFromList
+} from './_utilityHubBySigma';
 
-export const getCoatedCrunchOfMo
+export const getQuenchedCrossBranch =
+  (rhoCrossBranch,
+  askingMonth) => {
+
+  const coatedCrunchOfMonth =
+    getCoatedCrunchOfMonth(askingMonth);
+
+  const coatedBoltCrossBranch =
+    findItemByStringFromList(
+      coatedCrunchOfMonth
+      .coatedBoltCrossBranchSubsetOfMonth,
+      rhoCrossBranch,
+      'sigmaTargetCrossBranchNutForMonth'
+    )
+
+}
