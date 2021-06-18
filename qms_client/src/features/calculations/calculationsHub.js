62b79dea18aadfa0053b544320298a4d166251d2
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-06-18 11:09:04 +0800

    More on logos.

---
+++ qms_client/src/features/calculations/calculationsHub.js
@@ -0,0 +1,3 @@
+export * from './pearlForest/pearlForestHub';
+export * from './tauFiles/tauHub';
+export * from './utilityFiles/utilityHub';
--- qms_client/src/features/logos/bigCave/bigCaveGui.js
+++ qms_client/src/features/logos/bigCave/bigCaveGui.js
@@ -1,3 +1,53 @@
-function Foxy() {
-  return <h2>Foxy</h2>;
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export function Foxy() {
+  const classes = useStyles();
+
+  return (
+    <Container>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+      </Grid>
+    </div>
+    </Container>
+
+  );
 }
