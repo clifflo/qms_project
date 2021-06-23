66c52fda065cedc7eb71f0e004a3275456dde6eb
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-06-23 20:47:00 +0800

    More on orthodoxi sections.

---
+++ qms_client/src/features/orthodoxi/orthodoxiScripture_01001/orthodoxiSection_0100170.js
@@ -0,0 +1,121 @@
+export const orthodoxiSection_0100170 = {
+  orthodoxiChapterId: 'TC-01-001-70',
+  orthodoxiSectionId: 'TS-01-001-70',
+  orthodoxiSteps: [
+    {
+      orthodoxiStepCode: 1,
+      kalpanaContent: [
+        '先於室外秉簡當心，臨目扣齒三通，',
+        '存室內有紫雲之炁遍滿，又鬱鬱來冠兆身。',
+        '存玉童玉女侍經左右，三光寶芝洞煥室內。',
+        '存思畢，扣齒三通，念入戶呪。'
+      ],
+      chantContent: [
+        '天朗炁清，三光洞明，金房玉室，',
+        '五芝寶生，玄雲紫蓋，來映我身，',
+        '仙童玉女，為我致靈，九炁齊景，',
+        '三光同軿，上乘紫蓋，升入帝庭。'
+      ]
+    },
+    {
+      orthodoxiStepCode: 2,
+      kalpanaContent: [
+        '畢，先進右足入戶，三捻香，扣齒三通，祝。'
+      ],
+      chantContent: [
+        '玉華散景，九炁含烟，香雲密羅，',
+        '徑衝九天，侍香金童，傳言玉女，',
+        '上聞帝前，令臣長生，世為仙真，',
+        '隨心所願，莫不如言。'
+      ]
+    },
+    {
+      orthodoxiStepCode: 3,
+      onlyBuckleTimes: 3,
+      bowingTimes: 3,
+      kalpanaContent: [
+        '畢，先進右足入戶，三捻香，扣齒三通，祝。'
+      ],
+      chantContent: [
+        '玉華散景，九炁含烟，香雲密羅，',
+        '徑衝九天，侍香金童，傳言玉女，',
+        '上聞帝前，令臣長生，世為仙真，',
+        '隨心所願，莫不如言。'
+      ]
+    },
+    {
+      orthodoxiStepCode: 4,
+      faceDirection: 'North',
+      kalpanaContent: [
+        '面向北方，帝一對兆面前，',
+        '尊君狀如嬰兒。'
+      ],
+      chantContent: [
+        '微微玄宗門，煥朗徹空同，',
+        '至道由靜默，當見三素宮，',
+        '大道於此成，驂景策雲龍，',
+        '左右侍經真，玉女及玉童，',
+        '身濟不死津，解羅順靈風，',
+        '七祖返胎生，世為神仙宗。'
+      ]
+    },
+    {
+      orthodoxiStepCode: 5,
+      onlyBuckleTimes: 36,
+      faceDirection: 'North',
+      kalpanaContent: [
+        '畢，北向叩齒三十六通，',
+        '冥目內視，俯心祝之。'
+      ],
+      chantContent: [
+        '糞土小兆男生臣某，今有微言，',
+        '上啟玉清瓊元君，臣今登齋入室，',
+        '誦詠《上清大洞真經三十九章》，',
+        '伏願驅除魔試，辟却鬼病，',
+        '臣及七祖罪解福生。'
+      ]
+    },
+    {
+      orthodoxiStepCode: 6,
+      kalpanaContent: ['次祝之。'],
+      chantContent: [
+        '六天大魔王，受制幽寥無，',
+        '白距玃獸，赤甲毒龍，鎮守四維，',
+        '備衛九重，白鷹仰鳴，奮爪振豪，',
+        '萬精幽匿，滅邪破妖，天丁一怒，',
+        '五嶽振搖，風火征伐，九天啟朝。'
+      ]
+    },
+    {
+      orthodoxiStepCode: 7,
+      faceDirection: 'North',
+      firstBuckleTimes: 3,
+      secondBuckleTimes: 4,
+      kalpanaContent: [
+        '畢，北向四拜，三叩齒，長跪臨目，',
+        '叩齒二十四通，啟之。'
+      ],
+      chantContent: [
+        '糞土小兆男生臣某，',
+        '至心上啟高聖萬真太上大道君，',
+        '臣今入靖焚香，',
+        '誦詠《上清大洞真經三十九章》，',
+        '鍊神保藏，乞胃管華榮，聳身乘虛，',
+        '上拜帝庭，臣七祖罪釋，上生福堂。'
+      ]
+    },
+    {
+      orthodoxiStepCode: 8,
+      kalpanaContent: [
+        '畢，又四拜，長跪啟之。'
+      ],
+      charmContent: [
+        '臣某至心上啟中央黃老元素道君，',
+        '臣今入室焚香披詠《上清大洞真經三十九章》，',
+        '願臣靖室之內，芝英自生，',
+        '玉華寶曜，三光洞明，臣昇上清，',
+        '合同帝靈，使七祖罪解，克獲長生。'
+      ]
+    }
+  ]
+}
