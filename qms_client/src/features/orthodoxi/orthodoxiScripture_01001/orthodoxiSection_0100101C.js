df661e031a9b48083ad02aa3a2ee7fface37cf37
Cliff Augustine Lo <clifflo.the.diskjockey@gmail.com>
2021-06-27 21:53:52 +0800

    Nearly complete the 上清大洞真經.

---
+++ qms_client/src/features/orthodoxi/orthodoxiScripture_01001/orthodoxiSection_0100101C.js
@@ -0,0 +1,77 @@
+export const orthodoxiSection_0100101C = {
+  orthodoxiSectionId: 'OE:01-001-01C',
+  orthodoxiChapterId: 'OC:01-001-01',
+  orthodoxiSteps: [
+    {
+      orthodoxiStepId: 'OS:01-001-01C-01',
+      orthodoxiStepCode: 1,
+      orthodoxiBookPage: 'OB:01-006-C9',
+      kalpanaContent: [
+        '閉目默念，次心拜四方，',
+        '真思兆身坐五色雲中，雲氣覆蓋頭上，',
+        '想定。'
+      ],
+      prayerContent: [
+        '次就座，右顧東向，冥心叩齒九通，',
+        '陰誦太帝君素語內呪。'
+      ],
+      prayerActions: [
+        {
+          prayerActionOrder: 1,
+          prayerActionId: 'OP:01-001-01C-01-1',
+          prayerActionType: 'face_direction',
+          prayerActionValue: 'north'
+        },
+        {
+          prayerActionOrder: 2,
+          prayerActionId: 'OP:01-001-01C-01-2',
+          prayerActionType: 'neck_direction',
+          prayerActionValue: 'right'
+        },
+        {
+          prayerActionOrder: 3,
+          prayerActionId: 'OP:01-001-01C-01-3',
+          prayerActionType: 'mouth_buckle_times',
+          prayerActionValue: 9
+        }
+      ],
+      chantContent: [
+        '蒼元浩靈，少陽先生，九炁還肝，',
+        '使我魂寧，幽府結華，藏內鮮明，',
+        '鍊容固髓，返白為青，神化內發，',
+        '景登紫庭，敢有犯試，摧以流鈴，',
+        '上帝玉籙，名上太清。'
+      ]
+    },
+    {
+      orthodoxiStepId: 'OS:01-001-01C-02',
+      orthodoxiStepCode: 2,
+      orthodoxiBookPage: 'OB:01-007-A6',
+      prayerContent: [
+        '口引東方青陽之精青炁。',
+        '用鼻先取，以口極咽喉吸之。',
+        '餘四方並同。因閉炁九息，',
+        '咽炁九過，使布滿肝腑之中，',
+        '結作九神，青衣冠，狀如木星，',
+        '下布肝內，神面相向坐，順時吐息。'
+      ],
+      prayerActions: [
+        {
+          prayerActionOrder: 1,
+          prayerActionId: 'OP:01-001-01C-01-1',
+          prayerActionType: 'swallowing_air_color',
+          prayerActionValue: 'green'
+        },
+        {
+          prayerActionOrder: 2,
+          prayerActionId: 'OP:01-001-01C-01-2',
+          prayerActionType: 'swallowing_air_direction',
+          prayerActionValue: 'east'
+        },
+        {
+          prayerAction
+        }
+      ]
+    }
+  ]
+}
